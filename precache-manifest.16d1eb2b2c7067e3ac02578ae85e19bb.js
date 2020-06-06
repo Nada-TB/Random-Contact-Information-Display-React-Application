0740d347a0ed00a1f46522c20aac4554c526d369
Nada <nada.tebba@hotmail.fr>
2020-06-06 14:06:51 +0200

    Updates

---
+++ precache-manifest.16d1eb2b2c7067e3ac02578ae85e19bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f206315e0ab8b550cb87351bfa3194a",
+    "url": "/random-contacts-list/index.html"
+  },
+  {
+    "revision": "aebd08895dafcceba7cd",
+    "url": "/random-contacts-list/static/css/main.f9c77dcd.chunk.css"
+  },
+  {
+    "revision": "b928cd7a996bdbf4a94b",
+    "url": "/random-contacts-list/static/js/2.a8b73f6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-contacts-list/static/js/2.a8b73f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aebd08895dafcceba7cd",
+    "url": "/random-contacts-list/static/js/main.756e48e1.chunk.js"
+  },
+  {
+    "revision": "a2ca91f1fed3aa4e01b7",
+    "url": "/random-contacts-list/static/js/runtime-main.0fb31ee4.js"
+  }
+]);
